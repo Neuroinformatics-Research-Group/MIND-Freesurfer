989b150eee097b67e74888cbcda3c07f0cc0aa1a
S K Crockford <sarahcrockford@mac.com>
2026-08-25 13:46:12 +0100

    Add files via upload

---
+++ mind_run_configuration_public.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# mind_run_configuration.sh
+#
+# EDIT THIS FILE for every custom run. The SLURM submission script sources
+# it and stays untouched.
+
+
+# SLURM confirguration
+
+export scratch_path="your/scratch/directory/path/here"
+export email="your@email.ac.uk"
+export account=YOURACCOUNT
+export partition=PARTITION
+export job_name="your_job_name_here"
+
+# MIND arguments
+
+export data_dir="your/data/directory/path/here"
+export out_dir="path/where/you/plan/to/save/mind/networks"
+export projectname="PROJECTNAME"
+export parcellation="aparc" # "500_sym.aparc" "HCP.coarse.fsaverage.aparc" "HCP.fsaverage.aparc"
+export feature_list_to_use="['FA', 'MD', 'ICVF', 'ISOVF', 'OD', 'CT', 'MC', 'Vol', 'SD', 'SA']"
+export space="native" # "fsaverage6"
+export micro="micro" # "no-micro"
+export resample="no-resample" # "resample"
+export return_qc="return_qc" # "no-return_qc"
+export create_files="create_files" # "no-create_files"
+export test_mode="no-test_mode" # test_mode
+
